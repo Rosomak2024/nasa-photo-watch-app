ec74c5a014968cfcb30a7fdcb8ad52dd1402e935
Rosomak2024 <mbnet@o2.pl>
2026-04-09 12:42:33 +0200

    feat: implement photo fetching and display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <></>;
-}
+import { useState, useEffect } from "react";
 
-export default App;
+export default function App() {
+  const [photo, setPhoto] = useState(null);
+
+  function fetchRandom() {
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ&count=1"
+    )
+      .then((res) => res.json())
+      .then((data) => setPhoto(data[0]))
+      .catch((err) => console.error(err));
+  }
+
+  useEffect(() => {
+    fetchRandom();
+  }, []);
+
+  console.log(photo);
+  return (
+    <>
+      <div>
+        <button onClick={fetchRandom}>Next</button>
+        {photo && <img src={photo.url} alt="api_photo"></img>}
+      </div>
+    </>
+  );
+}
