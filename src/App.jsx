99b73c75bbf8960c97237436ad031bc99653aa31
Rosomak2024 <mbnet@o2.pl>
2026-04-14 11:04:17 +0200

    feat: improve photo display with loading state and reset button

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,15 @@ export default function App() {
   const [photo, setPhoto] = useState(null);
 
   function fetchRandom() {
-    const controller = new AbortController();
-
-    const timeoutId = setTimeout(() => {
-      controller.abort();
-    }, 5000);
-
     fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ&count=1",
-      { signal: controller.signal }
+      "https://api.nasa.gov/planetary/apod?api_key=RQlqLMnsMGXA1tDO50bIqQ2Sk30xXKVWmE9xNekJ&count=1"
     )
       .then((res) => res.json())
       .then((data) => {
-        clearTimeout(timeoutId);
         setPhoto(data[0]);
       })
       .catch((err) => {
-        if (err.name === "AbortError") {
-          console.log("fetch anulowany");
-        } else {
-          console.error(err);
-        }
+        console.error(err);
       });
   }
 
@@ -33,26 +21,32 @@ export default function App() {
     console.log("start fetch");
   }, []);
 
-  if (!photo) {
-    return <p>Loading...</p>;
-  }
-
-  console.log(photo);
-  return (
-    <>
-      <div className="display_api_container">
-        {photo.media_type === "image" ? (
-          <img src={photo.url} alt="api_photo" />
-        ) : (
-          <iframe
-            src={photo.url}
-            title={photo.title}
-            width="500"
-            height="300"
-          />
-        )}
+  if (photo) {
+    console.log(photo);
+    return (
+      <>
+        <div className="display_api_container">
+          {photo.media_type === "image" ? (
+            <img src={photo.url} alt="api_photo" />
+          ) : (
+            <iframe
+              src={photo.url}
+              title={photo.title}
+              width="500"
+              height="300"
+            />
+          )}
+          <h1>{photo.title}</h1>
+          <button onClick={fetchRandom}>Next</button>
+        </div>
+      </>
+    );
+  } else if (!photo) {
+    return (
+      <div>
+        <p>Loading...</p>
+        <button onClick={fetchRandom}>Reset Api fetch</button>
       </div>
-      <button onClick={fetchRandom}>Next</button>
-    </>
-  );
+    );
+  }
 }
