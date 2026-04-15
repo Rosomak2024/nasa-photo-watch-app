06fce461b5da9470a4f9f0dd6e666e46ed019d19
Rosomak2024 <mbnet@o2.pl>
2026-04-15 10:21:32 +0200

    fix: call fetchRandom function on reset button click fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ export default function App() {
         <button
           onClick={() => {
             setPhoto(null);
-            fetchRandom;
+            fetchRandom();
           }}
         >
           Reset
