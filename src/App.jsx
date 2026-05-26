1b9a92e6e910d55b9af4266cd1fd6fef93b91533
Rosomak2024 <mbnet@o2.pl>
2026-05-26 09:40:43 +0200

    feat: refactor photo fetching logic into custom hook and simplify App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import PhotoCard from "./components/PhotoCard";
 import Modal from "./components/Modal";
-const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+import useFetchPhotos from "./hooks/useFetchPhotos";
 
-export default function App() {
+function App() {
+  const { photos, loading, error } = useFetchPhotos();
 
-  const [number, setNumber] = useState(1);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [photos, setPhotos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-
-  async function fetchRandom() {
-    try {
-      setError(null);
-      setLoading(true);
-      const res = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=${number}`
-      );
-      const data = await res.json();
-      setPhotos(data);
-    } catch (err) {
-      console.error(err);
-      setError("Something went wrong. Try again.");
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchRandom();
-  }, []);
 
   if (loading) {
-    return <div className="loader"></div>;
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
   }
 
   return (
-    <>
-      <div className="display_api_container">
-      {photos.map((photo) => (
-    <PhotoCard
-      key={photo.url}
-      photo={photo}
-      setSelectedPhoto={setSelectedPhoto}
-    />
-  ))}
+    <main>
+      <div className="gallery">
+        {photos.map((photo) => (
+          <PhotoCard
+            key={photo.date}
+            photo={photo}
+            onClick={() => setSelectedPhoto(photo)}
+          />
+        ))}
       </div>
+
       {selectedPhoto && (
-  <Modal
-    selectedPhoto={selectedPhoto}
-    setSelectedPhoto={setSelectedPhoto}
-  />
-)}
-<input
-        type="number"
-        min={1}
-        max={5}
-        value={number}
-        onChange={(e) => setNumber(Number(e.target.value))}
-      />
-      <button onClick={fetchRandom}>Pobierz</button>
-      {error && <p className="error">{error}</p>}
-    </>
+        <Modal
+          photo={selectedPhoto}
+          onClose={() => setSelectedPhoto(null)}
+        />
+      )}
+    </main>
   );
 }
+
+export default App;
---
+++ src/hooks/useFetchPhotos.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export default function useFetchPhotos() {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      try {
+        setLoading(true);
+        setError("");
+
+        const res = await fetch(
+          `https://api.nasa.gov/planetary/apod?api_key=${
+            import.meta.env.VITE_NASA_API_KEY
+          }&count=12`
+        );
+
+        // if resposne not ok  throw error
+        if (!res.ok) {
+          throw new Error("Failed to fetch photos");
+        }
+
+        const data = await res.json();
+
+        // defensive check if no array throw error
+        if (!Array.isArray(data)) {
+          throw new Error("Invalid data format");
+        }
+
+        setPhotos(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPhotos();
+  }, []);
+
+  return {
+    photos,
+    loading,
+    error,
+  };
+}
